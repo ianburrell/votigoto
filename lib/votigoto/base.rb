56d07d2d2f6217ad4046c8299ad0eeffd3b92f3a
Ian Burrell <ianburrell@gmail.com>
2015-07-09 20:13:22 -0700

    Handle paging and multiple chunks

--- lib/votigoto/base.rb
+++ lib/votigoto/base.rb
@@ -4,33 +4,34 @@ class Votigoto::Base
     @ip = ip
     @mak = mak
   end
-  
+
   attr_reader :ip, :mak, :doc
-  
+
   def last_changed_date(reload=false)
     load(reload)
     Time.at(@doc.at("/tivocontainer/details/lastchangedate").inner_text.to_i(16))
   end
-  
+
   def shows(reload=false)
     load(reload)
-    return @shows if @shows
-    @shows = []
-    @doc.search("tivocontainer/item").each do |show|
-      @shows << Votigoto::Show.new(show)
-    end
     @shows
   end
+
   alias_method :to_a, :shows
-  
+
   def show(program_id,reload=false)
     show = shows(reload).select { |show| show.program_id == program_id.to_s }
     show.length == 1 ? show[0] : nil
   end
-  
+
+  def flush
+    @doc = nil
+    @shows = nil
+  end
+
 private
 
-  def getxml(uri = "TiVoConnect?Command=QueryContainer&Container=%2FNowPlaying&Recurse=Yes")
+  def getxml(uri)
     begin
       uri = URI.parse "https://tivo:#{@mak}@#{@ip}/#{uri}"
     rescue URI::InvalidURIError
@@ -53,17 +54,48 @@ private
             response.error!
           end
         end
-      end 
+      end
     end
     Hpricot(xml)
   end
-  
+
   def load(reload=false)
-    if reload
-      @doc = getxml()
-      @shows = nil
-    end
-    @doc ||= getxml()
+    flush() if reload
+    load_all() unless @doc
   end
-  
+
+  def load_all
+    offset = 0
+    count = 20
+    @shows = []
+    while true do
+      load_chunk(offset, count)
+      parse_shows()
+      offset = item_start + item_count
+      break if offset >= total_items
+    end
+  end
+
+  def load_chunk(offset, count)
+    @doc = getxml("TiVoConnect?Command=QueryContainer&Container=%2FNowPlaying&Recurse=Yes&AnchorOffset=#{offset}&ItemCount=#{count}")
+  end
+
+  def parse_shows
+    @doc.search("/tivocontainer/item").each do |show|
+      @shows << Votigoto::Show.new(show)
+    end
+  end
+
+  def total_items
+    @doc.at("/tivocontainer/details/totalitems").inner_text.to_i
+  end
+
+  def item_count
+    @doc.at("/tivocontainer/itemcount").inner_text.to_i
+  end
+
+  def item_start
+    @doc.at("/tivocontainer/itemstart").inner_text.to_i
+  end
+
 end
